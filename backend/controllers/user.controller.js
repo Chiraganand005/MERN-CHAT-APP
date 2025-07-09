c09d4d0538edfccc3e9661e8cda50ae01197c2d5
Chirag Anand <chiraga087@gmail.com>
2025-07-09 17:51:17 +0530

    completed frontend of the Project

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,17 +1,14 @@
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
-    try {
-        const loggedInUserId = req.user._id; // Assuming user ID is stored in req.user
-        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
-        
-        res.ststus(200).json({
-            message: "Users fetched successfully",
-            users: filteredUsers,
-        });
-    } catch (error) {
-        console.error("Error fetching users for sidebar:", error.message);
-        res.status(500).json({ message: "Internal server error" });
-        
-    }
+	try {
+		const loggedInUserId = req.user._id;
+
+		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
